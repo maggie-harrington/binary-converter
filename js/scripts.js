d4ea07898c578267ee8dc5d2dd29b7a420cd9f17
Maggie Harrington and Matt Kelley <maggie@>
2017-01-18 13:22:47 -0800

    add an if statement to decide which base the number will be converted to

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,15 @@ $(document).ready(function() {
     event.preventDefault();
     $("#output").empty();
     var item = parseInt($("#tbox").val())
-    item = item.toString(2);
+    console.log("value is " + $("input:radio[name=base]:checked").val());
+    if ($("input:radio[name=base]:checked").val() === "binary") {
+      item = item.toString(2);
+    } else if ($("input:radio[name=base]:checked").val() === "trinary") {
+      item = item.toString(3);
+    } else if ($("input:radio[name=base]:checked").val() === "hex") {
+      item = item.toString(16);
+    }
+
     $("#output").append("<p>" + item + "</p>");
   });
 });
