4ea67283d47f407251798c101b664491d95a30a5
Maggie Harrington and Matt Kelley <maggie@>
2017-01-18 13:28:55 -0800

    added a if state checking if the inputted value is a number

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,11 @@ $(document).ready(function() {
   $("form#binary-form").submit(function(event) {
     event.preventDefault();
     $("#output").empty();
-    var item = parseInt($("#tbox").val())
-    console.log("value is " + $("input:radio[name=base]:checked").val());
-    if ($("input:radio[name=base]:checked").val() === "binary") {
+    var item = parseInt($("#tbox").val());
+
+    if (item.toString() === "NaN") {
+      item = "Please Enter A Number"
+    } else if ($("input:radio[name=base]:checked").val() === "binary") {
       item = item.toString(2);
     } else if ($("input:radio[name=base]:checked").val() === "trinary") {
       item = item.toString(3);
